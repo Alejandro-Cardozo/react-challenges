28420f64874a8f989099d2e54a923021b5ae3c44
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-01-24 15:00:35 -0300

    Basic components/functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import TeamMembers from './components/TeamMembers';
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const response = await fetch('https://randomuser.me/api/?results=8&inc=name,dob,email,phone,picture,location,nat,login');
+      const data = await response.json();
+      setUsers(data.results);
+    };
+
+    fetchUser();
+  }, []);
+
+  if (!users.length) {
+    return <div className='home'><p>Loading...</p></div>
+  }
+
+  return (
+    <div className='home'>
+      <h1>Meet our team</h1>
+      <TeamMembers users={users} />
+    </div>
+  );
 }
 
 export default App;
