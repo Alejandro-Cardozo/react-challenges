70c4f28cbd2b318b478d4b33d7e71945660b03f7
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-02-12 10:36:31 -0300

    WeatherDetails component and styles

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,7 +1,35 @@
-const WeatherDetails = () => {
-  return (
-    <div>WeatherDetails</div>
-  )
-}
+// Styles
+import classes from './WeatherDetails.module.css';
 
-export default WeatherDetails
+const WeatherDetails = ({ weatherData }) => {
+  const {
+    humidity,
+    dewpoint,
+    apparent_temperature,
+    visibility,
+    windspeed,
+    winddirection,
+  } = weatherData;
+
+  return (
+    <div className={classes.details}>
+      <div className={classes.item}>
+        <p>Sensación Térmica {apparent_temperature}°C</p>
+      </div>
+      <div className={classes.item}>
+        <p>Viento <span>-&gt;</span> {windspeed} km/h</p>
+      </div>
+      <div className={classes.item}>
+        <p>Visibilidad {visibility/1000} km</p> 
+      </div>
+      <div className={classes.item}>
+        <p>Humedad {humidity}%</p>
+      </div>
+      <div className={classes.item}>
+        <p>Punto de Rocío {dewpoint}°C</p>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherDetails;
