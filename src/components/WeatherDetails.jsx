b3de48562360e3a1d45e93a438fa676fd9756b7b
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-02-12 11:18:18 -0300

    Wind speed indicator and some styling adjustments

--- src/components/WeatherDetails.jsx
+++ src/components/WeatherDetails.jsx
@@ -1,3 +1,6 @@
+// Icons
+import { NavigationArrow } from 'phosphor-react';
+
 // Styles
 import classes from './WeatherDetails.module.css';
 
@@ -14,19 +17,46 @@ const WeatherDetails = ({ weatherData }) => {
   return (
     <div className={classes.details}>
       <div className={classes.item}>
-        <p>Sensación Térmica {apparent_temperature}°C</p>
+        <p>
+          Viento
+          <span className={classes.value}>
+            <span>
+              <NavigationArrow
+                className={classes.navArrow}
+                size={16}
+                color='#eeeeec'
+                weight='fill'
+                style={{
+                  transform: `rotate(calc(135deg + ${winddirection}deg))`,
+                }}
+              />
+            </span>{' '}
+            {windspeed}
+          </span>{' '}
+          km/h
+        </p>
       </div>
       <div className={classes.item}>
-        <p>Viento <span>-&gt;</span> {windspeed} km/h</p>
+        <p>
+          Humedad <span className={classes.value}>{humidity}</span>%
+        </p>
       </div>
       <div className={classes.item}>
-        <p>Visibilidad {visibility/1000} km</p> 
+        <p>
+          Visibilidad <span className={classes.value}>{visibility / 1000}</span>{' '}
+          km
+        </p>
       </div>
       <div className={classes.item}>
-        <p>Humedad {humidity}%</p>
+        <p>
+          Sensación Térmica{' '}
+          <span className={classes.value}>{apparent_temperature}</span>°C
+        </p>
       </div>
       <div className={classes.item}>
-        <p>Punto de Rocío {dewpoint}°C</p>
+        <p>
+          Punto de Rocío <span className={classes.value}>{dewpoint}</span>°C
+        </p>
       </div>
     </div>
   );
