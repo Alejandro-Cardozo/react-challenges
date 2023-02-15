74ebf40ec4da5fb957d5869e23fa9cba4664a262
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-02-15 11:15:03 -0300

    Displaying the current track data and progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,5 +1,14 @@
+// Styles
+import classes from './ProgressBar.module.css';
+
 const ProgressBar = () => {
-  return <div>ProgressBar</div>;
+  return (
+    <div className={classes.progress}>
+      <span className='time current'>00:00</span>
+      <input type='range' />
+      <span className='time'>03:34</span>
+    </div>
+  );
 };
 
 export default ProgressBar;
