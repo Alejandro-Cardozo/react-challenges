99ec5130310d584375a312f08ce8a3fbe47cb974
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-02-21 14:30:04 -0300

    apply better styles to the card component

--- support-desk-app/src/App.jsx
+++ support-desk-app/src/App.jsx
@@ -3,23 +3,41 @@ import TeamMembers from './components/TeamMembers';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
-      const response = await fetch(
-        'https://randomuser.me/api/?results=8&inc=name,dob,email,phone,picture,location,nat,login&nat=us,fr,gb,au,br,de,es,ir,mx,nz'
-      );
-      const data = await response.json();
-      setUsers(data.results);
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          'https://randomuser.me/api/?results=8&inc=name,gender,dob,email,phone,picture,location,nat,login&nat=us,fr,gb,br,de,es,mx'
+        );
+        const data = await response.json();
+        setUsers(data.results);
+      } catch (error) {
+        console.log(error.message ? error.message : 'Something went wrong...');
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
     };
 
     fetchUser();
   }, []);
 
-  if (!users.length) {
+  if (isLoading) {
     return (
       <div className='home'>
         <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (!isLoading && hasError) {
+    return (
+      <div className='home'>
+        <p>Oops, something went wrong. Please try to reload the page.</p>
       </div>
     );
   }
--- support-desk-app/src/components/Card.jsx
+++ support-desk-app/src/components/Card.jsx
@@ -4,34 +4,45 @@ const Card = ({ user }) => {
   if (!user) {
     return <p>Loading...</p>;
   }
-
   const userData = {
-    completeName:
-      `${user.name.title}. ${user.name.first} ${user.name.last}` || '',
+    completeName: `${user.name.first} ${user.name.last}` || '',
     age: user.dob.age || 0,
     email: user.email || '',
     number: user.phone || '',
     picture: user.picture.large || '',
     country: user.location.country || '',
+    gender: user.gender || '',
     flag: `https://flagcdn.com/w80/${user.nat.toLowerCase()}.png`,
   };
 
   return (
     <div className={classes.card}>
-      <img src={userData.picture} alt={userData.completeName} />
-      <img src={userData.flag} width='80' alt={userData.country} />
-      <div className='content'>
-        <h3 className='title'>
-          {userData.completeName} <span>({userData.age})</span>
-        </h3>
-        <p className='email'>
-          {userData.email.replace('example', 'ourteam')}
-        </p>
+      <img
+        className={classes.picture}
+        src={userData.picture}
+        alt={userData.completeName}
+      />
+      <img
+        className={classes.flag}
+        src={userData.flag}
+        width='80'
+        alt={userData.country}
+      />
+      <div className={classes.content}>
+        <span
+          className={`${classes.age} ${
+            userData.gender === 'female' ? classes.female : classes.male
+          }`}
+        >
+          {userData.gender === 'female' ? '♀' : '♂'} {userData.age}
+        </span>
+        <h3 className={classes.title}>{userData.completeName} </h3>
+        <p className='email'>{userData.email.replace('example', 'ourteam')}</p>
         <p className='number'>{userData.number}</p>
       </div>
       <div className='actions'>
         <a href={`mailto:${userData.email.replace('example', 'ourteam')}`}>
-          <button>send a message</button>
+          <button className={classes.btn}>Send a message</button>
         </a>
       </div>
     </div>
