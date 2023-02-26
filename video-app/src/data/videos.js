a4036a0d969b0d88a28fb6d2660071bb9644c78a
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-02-26 14:14:57 -0300

    Add new videos and fix minor style issues

--- video-app/src/data/videos.js
+++ video-app/src/data/videos.js
@@ -27,6 +27,62 @@ const videos = [
     youtubeId: 'ZS_kXvOeQ5Y',
     thumbnail: 'https://img.youtube.com/vi/ZS_kXvOeQ5Y/sddefault.jpg',
   },
+  {
+    id: 'v5',
+    title: 'Linux Server Setup & MERN Deployment',
+    url: 'https://www.youtube.com/embed/7aRjGIhwyQM',
+    youtubeId: '7aRjGIhwyQM',
+    thumbnail: 'https://img.youtube.com/vi/7aRjGIhwyQM/sddefault.jpg',
+  },
+  {
+    id: 'v6',
+    title: 'Build An AI Image Generator With OpenAI & Node.js',
+    url: 'https://www.youtube.com/embed/fU4o_BKaUZE',
+    youtubeId: 'fU4o_BKaUZE',
+    thumbnail: 'https://img.youtube.com/vi/fU4o_BKaUZE/sddefault.jpg',
+  },
+  {
+    id: 'v7',
+    title: 'CSS Grid Crash Course',
+    url: 'https://www.youtube.com/embed/0xMQfnTU6oo',
+    youtubeId: '0xMQfnTU6oo',
+    thumbnail: 'https://img.youtube.com/vi/0xMQfnTU6oo/sddefault.jpg',
+  },
+  {
+    id: 'v8',
+    title: 'Remix Crash Course',
+    url: 'https://www.youtube.com/embed/d_BhzHVV4aQ',
+    youtubeId: 'd_BhzHVV4aQ',
+    thumbnail: 'https://img.youtube.com/vi/d_BhzHVV4aQ/sddefault.jpg',
+  },
+  {
+    id: 'v9',
+    title: 'React Security Crash Course',
+    url: 'https://www.youtube.com/embed/ibKXoU1I3cE',
+    youtubeId: 'ibKXoU1I3cE',
+    thumbnail: 'https://img.youtube.com/vi/ibKXoU1I3cE/sddefault.jpg',
+  },
+  {
+    id: 'v10',
+    title: 'Form Validation in React',
+    url: 'https://www.youtube.com/embed/k9JIxbQRnWs',
+    youtubeId: 'k9JIxbQRnWs',
+    thumbnail: 'https://img.youtube.com/vi/k9JIxbQRnWs/sddefault.jpg',
+  },
+  {
+    id: 'v11',
+    title: 'CSS Architecture in React',
+    url: 'https://www.youtube.com/embed/SYi_kpaAqrA',
+    youtubeId: 'SYi_kpaAqrA',
+    thumbnail: 'https://img.youtube.com/vi/SYi_kpaAqrA/sddefault.jpg',
+  },
+  {
+    id: 'v12',
+    title: 'Figma to React with CSS Modules',
+    url: 'https://www.youtube.com/embed/CHtMGW5HIt4',
+    youtubeId: 'CHtMGW5HIt4',
+    thumbnail: 'https://img.youtube.com/vi/CHtMGW5HIt4/sddefault.jpg',
+  },
 ];
 
 export default videos;
