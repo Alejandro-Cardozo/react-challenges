0eb70a05bdd0bbea10f7df4c7b510e0110461459
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-03-01 09:35:06 -0300

    Fix modal styles

--- support-desk-app/src/components/UI/Modal.jsx
+++ support-desk-app/src/components/UI/Modal.jsx
@@ -7,14 +7,17 @@ const Modal = () => {
     <Overlay>
       <div className={classes.modal}>
         <div className={classes.modalBox}>
-          <h3 className={classes.modalTitle}>Congratulations random Internet user!</h3>
-          <p className={classes.modalText}>
-            You&apos;ve been selected for a chance to get one year of subscription to use Wikipedia
-            for free!
-          </p>
+          <h3 className={classes.modalTitle}>Send a message to ...</h3>
+          <form className={classes.modalForm}>
+            <label htmlFor='name'>Your name</label>
+            <input type='text' name='name' id='name' required autoFocus maxLength={30} />
+            <label htmlFor='message'>Message</label>
+            <textarea type='text' name='message' id='message' rows={5} required maxLength={150}></textarea>
+          </form>
           <div className={classes.modalActions}>
-            <button className={classes.modalBtn}>Yay!</button>
+            <button className={`${classes.btn} ${classes.modalBtn}`}>Send Message</button>
           </div>
+          <button className={`${classes.btn} ${classes.cancelBtn}`}>X</button>
         </div>
       </div>
     </Overlay>
