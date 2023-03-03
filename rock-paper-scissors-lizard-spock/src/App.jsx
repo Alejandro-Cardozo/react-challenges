db755a489801e1abced7865d63102c3bdf0eb4b6
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-03-03 09:04:36 -0300

    Setup the initial project

---
+++ rock-paper-scissors-lizard-spock/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='center'>
+      <h1>Rock, Paper, Scissors, Lizard, Spock</h1>
+      <div>
+        <div>
+          <div>
+            <div>Player 1: 0</div>
+            <div />
+          </div>
+          <div>
+            <div>Player 2: 0</div>
+            <div />
+          </div>
+        </div>
+        <div>
+          <button>Rock</button>
+          <button>Paper</button>
+          <button>Scissors</button>
+          <button>Lizard</button>
+          <button>Spock</button>
+        </div>
+        <h2>Player 1 Wins!</h2>
+      </div>
+    </div>
+  )
+}
+
+export default App
