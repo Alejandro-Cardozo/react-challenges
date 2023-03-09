c2106078baea17bd4b52912231267faaec8d5781
Alejandro Cardozo <uri.cardozo@gmail.com>
2023-03-09 17:09:06 -0300

    Apply final styling

--- audio-player-app/src/components/ListTrackItem.jsx
+++ audio-player-app/src/components/ListTrackItem.jsx
@@ -1,14 +1,40 @@
-import classes from './ListTrackItem.module.css'
+import { useEffect, useMemo, useState } from 'react'
+import classes from './ListTrackItem.module.css'// Icons
+import { BsMusicNoteBeamed } from 'react-icons/bs'
+// Helpers
+import { formatTime } from '../helpers/helpers'
 
 const ListTrackItem = ({ track }) => {
+  const [duration, setDuration] = useState(0)
+
+  useEffect(() => {
+    const audio = new Audio(track.src)
+    audio.addEventListener('loadedmetadata', () => {
+      setDuration(audio.duration)
+    })
+  }, [formatTime, track.src])
+
   return (
     <li className={classes.trackItem}>
-      <img src={track.thumbnail} alt={track.title} />
-      <div>
+      {/* <img src={track.thumbnail} alt={track.title} /> */}
+      <div className={classes['audio-image']}>
+          {track.thumbnail
+            ? (
+              <img src={track.thumbnail} alt={track.title} />
+              )
+            : (
+              <div className={classes['icon-wrapper']}>
+                <span className={classes['audio-icon']}>
+                  <BsMusicNoteBeamed />
+                </span>
+              </div>
+              )}
+        </div>
+      <div className={classes.info}>
         <h3>{track.title}</h3>
         <p>{track.author}</p>
       </div>
-      <p>00:00</p>
+      <p className={classes.duration}>{formatTime(duration)}</p>
     </li>
   )
 }
